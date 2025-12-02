6240a5b4e95132194af390e7c9ae5726a01b871b
Enryco Martins <enrycomartins@gmail.com>
2025-12-02 16:51:26 -0300

    Metodos para o cpf

--- springboot-app/src/main/java/net/pi/springboot_app/model/Usuario.java
+++ springboot-app/src/main/java/net/pi/springboot_app/model/Usuario.java
@@ -43,4 +43,7 @@ public class Usuario {
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
 }
